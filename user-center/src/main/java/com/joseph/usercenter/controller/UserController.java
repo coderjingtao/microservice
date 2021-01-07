15c8dd9a6e35767ba669f2464bc5061ddfa11282
Joseph Liu <Joseph.Liu@Newaim01.com>
2021-01-07 18:16:10 +1100

    add Ribbon

--- user-center/src/main/java/com/joseph/usercenter/controller/UserController.java
+++ user-center/src/main/java/com/joseph/usercenter/controller/UserController.java
@@ -3,6 +3,7 @@ package com.joseph.usercenter.controller;
 import com.joseph.usercenter.domain.entity.user.User;
 import com.joseph.usercenter.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +15,14 @@ import java.util.Date;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping("/{id}")
     public User findById(@PathVariable Integer id){
+        log.info("我被其他微服务请求了 ... ");
         return this.userService.findById(id);
     }
 
